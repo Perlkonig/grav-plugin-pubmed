019486efd5b5323ecd2953df4f9b2f316bf04ab1
Aaron Dalton <aaron@daltons.ca>
2016-09-04 22:15:08 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,96 @@
+# Pubmed Plugin
+
+The **Pubmed** Plugin is for [Grav CMS](http://github.com/getgrav/grav). It uses the [National Center for Biotechnology Information's](https://www.ncbi.nlm.nih.gov/) [E-utils API](https://www.ncbi.nlm.nih.gov/books/NBK25500/) to access pubmed information about publications. This is accomplished by passing UIDs via a special shortcode.
+
+## Installation
+
+Installing the Pubmed plugin can be done in one of two ways. The GPM (Grav Package Manager) installation method enables you to quickly and easily install the plugin with a simple terminal command, while the manual method enables you to do so via a zip file.
+
+### GPM Installation (Preferred)
+
+The simplest way to install this plugin is via the [Grav Package Manager (GPM)](http://learn.getgrav.org/advanced/grav-gpm) through your system's terminal (also called the command line).  From the root of your Grav install type:
+
+    bin/gpm install pubmed
+
+This will install the Pubmed plugin into your `/user/plugins` directory within Grav. Its files can be found under `/your/site/grav/user/plugins/pubmed`.
+
+### Manual Installation
+
+To install this plugin, just download the zip version of this repository and unzip it under `/your/site/grav/user/plugins`. Then, rename the folder to `pubmed`. You can find these files either on [GitHub](https://github.com/Perlkonig/grav-plugin-pubmed) or via [GetGrav.org](http://getgrav.org/downloads/plugins#extras).
+
+You should now have all the plugin files under
+
+    /your/site/grav/user/plugins/pubmed
+	
+> NOTE: This plugin is a modular component for Grav which requires [Grav](http://github.com/getgrav/grav), the [Error](https://github.com/getgrav/grav-plugin-error) and [Problems](https://github.com/getgrav/grav-plugin-problems) plugins, and a theme to be installed in order to operate.
+
+## Usage
+
+The shortcode contains the following parts:
+
+  - The string `[pubmed>`
+  - Followed by one of the three commands:
+    - `short`
+    - `long`
+    - `summary`
+  - Followed by a colon (`:`)
+  - Followed by a list of UIDs separated by commas (no spaces!)
+  - And finally a closing bracket (`]`)
+
+The following are valid shortcodes:
+
+  - `[pubmed>short:11111]`
+  - `[pubmed>long:11111,22222]`
+  - `[pubmed>summary:11111,22222,33333]`
+
+These are block elements. Each shortcode is replaced by a `<div>` containing the data.
+
+The expanded shortcode *is* cached.
+
+## Configuration
+
+The default configuration is as follows:
+
+```
+enabled: true
+active: false
+
+# Formats are a string that only contain 
+#   - valid field names enclosed in square brackets
+#   - line breaks encoded as the string "\n"
+#   - other literals (printed as is)
+#
+# Valid field names are as follows (case sensitive!)
+#   - title
+#   - authors_long
+#   - authors_short
+#   - journal
+#   - volume
+#   - pages
+#   - date
+
+formats:
+  short: "[authors_short] [journal] [volume] [pages] [date]"
+  long: "[title]\n[authors_long] [journal] [volume] [pages] [date]"
+  author_sep: ", "  # string that is inserted between list of authors when using [authors_long]
+```
+
+To change the configuration, copy `pubmed.yaml` from the `plugins/pubmed` folder into your `config/plugins` folder. That way, if the plugin gets updated, your custom configuration is not lost.
+
+- `enabled` tells Grav to run the plugin. If you set this to `false`, the plugin will be fully disabled and the shortcodes will print as is.
+
+- `active` makes it possible to only run the plugin on specific pages. In the header of the page that includes the shortcode, put the following in the header:
+
+  ```
+  pubmed:
+    active: true
+  ```
+
+  Alternatively, you can set `active` to `true` in the base config and the plugin will search all pages for the shortcode.
+
+- `formats` gives you flexibility over how the `short` and `long` versions are formatted. The embedded instructions are hopefully self-explanatory.
+
+## Customization
+
+Each element of the citation has a class assigned to it for easy styling. Copy the `pubmed.css` file from the `plugins/pubmed/assets` folder and add it to your theme's ``assets`` folder. Edit as you see fit.
+
---
+++ pubmed.php
@@ -0,0 +1,167 @@
+<?php
+namespace Grav\Plugin;
+
+use Grav\Common\Plugin;
+use RocketTheme\Toolbox\Event\Event;
+
+/**
+ * Class PubmedPlugin
+ * @package Grav\Plugin
+ */
+class PubmedPlugin extends Plugin
+{
+    private $pubmed = null;
+
+    /**
+     * @return array
+     *
+     * The getSubscribedEvents() gives the core a list of events
+     *     that the plugin wants to listen to. The key of each
+     *     array section is the event that the plugin listens to
+     *     and the value (in the form of an array) contains the
+     *     callable (or function) as well as the priority. The
+     *     higher the number the higher the priority.
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            'onPluginsInitialized' => ['onPluginsInitialized', 0]
+        ];
+    }
+
+    /**
+     * Initialize the plugin
+     */
+    public function onPluginsInitialized()
+    {
+        // Don't proceed if we are in the admin plugin
+        if ($this->isAdmin()) {
+            return;
+        }
+
+        // Enable the main event we are interested in
+        $this->enable([
+            'onPageInitialized' => ['onPageInitialized', 0],
+        ]);
+    }
+
+    public function onPageInitialized()
+    {
+        if ($this->isAdmin()) {
+            $this->active = false;
+            return;
+        }
+
+        $defaults = (array) $this->config->get('plugins.pubmed');
+        dump($defaults);
+        /** @var Page $page */
+        $page = $this->grav['page'];
+        if (isset($page->header()->pubmed)) {
+            dump($page->header()->pubmed);
+            $this->config->set('plugins.pubmed', array_merge($defaults, $page->header()->pubmed));
+        }
+        if ($this->config->get('plugins.pubmed.active')) {
+            $this->enable([
+                'onTwigSiteVariables' => ['onTwigSiteVariables', 0],
+                'onMarkdownInitialized' => ['onMarkdownInitialized', 0],
+            ]);
+        }
+    }
+
+    public function onTwigSiteVariables()
+    {
+        $this->grav['assets']
+            ->add('theme://assets/pubmed.css');
+    }
+
+    /**
+     * Do some work for this event, full details of events can be found
+     * on the learn site: http://learn.getgrav.org/plugins/event-hooks
+     *
+     * @param Event $e
+     */ 
+    public function onMarkdownInitialized(Event $e)
+    {
+        require_once(__DIR__.'/classes/pubmed.class.php');
+        $markdown = $e['markdown'];
+        $markdown->addBlockType('[', 'PubMed', true, false);
+        $this->pubmed = new PubMed();
+
+        $markdown->blockPubMed = function($Line) {
+            if (preg_match('/\[pubmed\>(short|long|summary)\:(\d+(\,\d+)*?)\]/', $Line['text'],  $matches)) {
+                $cmd = $matches[1];
+                $ids = $matches[2];
+
+                //Fetch the data
+                $this->pubmed->get_summary($ids);
+
+                $recs = '';
+                if ($cmd === 'summary') {
+                    /*
+                    $rec = array (
+                        'element' => array (
+                            'name' => 'pre',
+                            'text' => $pubmed->response,
+                        ),
+                    );
+                    array_push($recs, $rec);
+                    */
+                    $recs = '<pre>'.$this->pubmed->response.'</pre>';
+                } else {
+                    //Build individual records
+                    $format = $this->config->get('plugins.pubmed.formats.' . $cmd);
+                    foreach ($this->pubmed->extract as $record) {
+                        $text = $format;
+                        $text = str_replace('\n', '<br>', $text);
+                        foreach (['title', 'authors_short', 'authors_long', 'journal', 'volume', 'pages', 'date'] as $field) {
+                            $replacement = '';
+                            if ($field === 'authors_short') {
+                                $authorstr = htmlspecialchars($record['authors'][0]);
+                                if (count($record['authors']) > 1) {
+                                    $authorstr = $authorstr . ' <span class="etal">et al.</span>';
+                                }
+                                $replacement = $authorstr;
+                            } elseif ($field === 'authors_long') {
+                                $sep = $this->config->get('plugins.pubmed.formats.author_sep', ', ');
+                                $replacement = htmlspecialchars(implode($sep, $record['authors']));
+                            } else {
+                                $replacement = htmlspecialchars($record[$field]);
+                            }
+                            $text = str_replace(
+                                '[' . $field . ']', 
+                                '<span class="' . $field . '">' . $replacement . '</span>', 
+                                $text);
+                        }
+                        $recs = $recs . '<p>' . $text . '</p>' . "\n";
+                        /*
+                        $rec = array (
+                            'element' => array (
+                                'name'=> 'p',
+                                'text' => $text,
+                            ),
+                        );
+                        array_push($recs, $rec);
+                        */
+                    }
+                }
+
+                //return '<div class="pubmed">'.$recs.'</div>';
+                $Block = array (
+                    'element' => array (
+                        'name' => 'div',
+                        'attributes' => array (
+                            'class' => 'pubmed',
+                        ),
+                        'text' => $recs,
+                    ),
+                );
+
+                return $Block;
+            }
+        };
+
+        
+
+
+    }
+}
